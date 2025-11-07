64febf913bd15ef92c35cea35b40a2550e4456f8
Martin Helmich <m.helmich@mittwald.de>
2025-11-07 10:29:03 +0100

    chore: formatting

--- src/MittwaldChatMessageIterator.php
+++ src/MittwaldChatMessageIterator.php
@@ -7,33 +7,35 @@ use Drupal\ai\OperationType\Chat\StreamedChatMessageIterator;
 /**
  * OpenAI Chat message iterator.
  */
-class MittwaldChatMessageIterator extends StreamedChatMessageIterator {
+class MittwaldChatMessageIterator extends StreamedChatMessageIterator
+{
 
-  /**
-   * {@inheritdoc}
-   */
-  public function doIterate(): \Generator {
-    foreach ($this->iterator->getIterator() as $data) {
-      $metadata = $data->usage ? $data->usage->toArray() : [];
-      $message = $this->createStreamedChatMessage(
-        $data->choices[0]->delta->role ?? '',
-        $data->choices[0]->delta->content ?? '',
-        $metadata,
-        $data->choices[0]->delta->toolCalls ?? NULL,
-        $data->toArray(),
-      );
-      if ($data->usage !== NULL) {
-        $message->setInputTokenUsage($data->usage->promptTokens ?? 0);
-        $message->setOutputTokenUsage($data->usage->completionTokens ?? 0);
-        $message->setTotalTokenUsage($data->usage->totalTokens ?? 0);
-        $message->setReasoningTokenUsage($data->usage->completionTokenDetails->reasoningTokens ?? 0);
-        $message->setCachedTokenUsage($data->usage->completionTokenDetails->cachedTokens ?? 0);
-      }
-      if (isset($data->choices[0]->finishReason)) {
-        $this->setFinishReason($data->choices[0]->finishReason);
-      }
-      yield $message;
+    /**
+     * {@inheritdoc}
+     */
+    public function doIterate(): \Generator
+    {
+        foreach ($this->iterator->getIterator() as $data) {
+            $metadata = $data->usage ? $data->usage->toArray() : [];
+            $message  = $this->createStreamedChatMessage(
+                $data->choices[0]->delta->role ?? '',
+                $data->choices[0]->delta->content ?? '',
+                $metadata,
+                $data->choices[0]->delta->toolCalls ?? null,
+                $data->toArray(),
+            );
+            if ($data->usage !== null) {
+                $message->setInputTokenUsage($data->usage->promptTokens ?? 0);
+                $message->setOutputTokenUsage($data->usage->completionTokens ?? 0);
+                $message->setTotalTokenUsage($data->usage->totalTokens ?? 0);
+                $message->setReasoningTokenUsage($data->usage->completionTokenDetails->reasoningTokens ?? 0);
+                $message->setCachedTokenUsage($data->usage->completionTokenDetails->cachedTokens ?? 0);
+            }
+            if (isset($data->choices[0]->finishReason)) {
+                $this->setFinishReason($data->choices[0]->finishReason);
+            }
+            yield $message;
+        }
     }
-  }
 
 }
